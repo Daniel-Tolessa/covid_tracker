8a6068c7c2336cfb6fbeb9feed8c2fcce056764b
Daniel <daniaddis21@gmail.com>
2020-08-19 10:52:26 -0400

    'error'

--- covid_tracker/settings.py
+++ covid_tracker/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenose.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'covid_tracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
